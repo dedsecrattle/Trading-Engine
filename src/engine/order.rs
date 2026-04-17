932055498e2b8166b73364e68bcc5687341a170a
Prabhat Kumar <itsprabxxx@gmail.com>
2026-04-18 00:11:23 +0800

    add order

---
+++ src/engine/order.rs
@@ -0,0 +1,44 @@
+use chrono::{DateTime, Utc};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Side {
+    Buy,
+    Sell,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum OrderType {
+    Limit,
+    Market,
+}
+
+#[derive(Debug)]
+pub struct Order {
+    id: u64,
+    side: Side,
+    order_type: OrderType,
+    price: Option<u64>,
+    quantity: u64,
+    remaining: u64,
+    timestamp: DateTime<Utc>,
+}
+
+impl Order {
+    pub fn new(
+        side: Side,
+        order_type: OrderType,
+        price: Option<u64>,
+        quantity: u64,
+        timestamp: DateTime<Utc>,
+    ) -> Order {
+        Order {
+            id: 1,
+            side,
+            order_type,
+            price,
+            quantity,
+            timestamp,
+            remaining: quantity,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use chrono::Utc;
+
+mod engine;
+use engine::order::{Order, OrderType, Side};
+
 fn main() {
-    println!("Hello, world!");
+    let buy_order = Order::new(Side::Buy, OrderType::Limit, Some(100), 10, Utc::now());
+    println!("{:?}", buy_order)
 }
